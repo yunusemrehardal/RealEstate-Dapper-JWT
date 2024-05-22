dce84de0819d35627e24f3b09d3414dc422ca1ab
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2024-05-22 17:27:54 +0300

    HomePage almost

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBottomGridComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBottomGridComponentPartial.cs
@@ -1,6 +1,27 @@
-﻿namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.BottomGridDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
-    public class _DefaultBottomGridComponentPartial
+    public class _DefaultBottomGridComponentPartial:ViewComponent
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultBottomGridComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44334/api/BottomGrids");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
