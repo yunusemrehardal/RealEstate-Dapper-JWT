67c4c7f8c80924b9c305e2ad6d9e5d6383d75bc4
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2024-05-23 14:01:24 +0300

    Home Page Done-Admin Start

--- RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutHeadComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutHeadComponentPartial.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+namespace RealEstate_Dapper_UI.ViewComponents.AdminLayout
 {
-    public class _DefaultOurClientsComponentPartial : ViewComponent
+    public class _AdminLayoutHeadComponentPartial : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
